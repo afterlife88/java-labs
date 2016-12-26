7c5baa00e2d1140ff596779ff1caa0bb6c56978a
Andrew Iasko <andrii.iasko@gmail.com>
2016-12-26 17:48:09 +0200

    Added inner excpetion stuff for get cause

--- Excpetion_Lab/src/Main.java
+++ Excpetion_Lab/src/Main.java
@@ -3,15 +3,18 @@ import services.CalculationService;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
         // Call service
         CalculationService calcService = new CalculationService();
         try {
             Boolean result = calcService.IsPlusResult(null, 1);
-            System.out.println("Result is positive? " +  result.toString());
-        } catch (ServiceException ex){
-            System.out.println(ex);}
-    }
-}
+            System.out.println("Result is positive? " + result.toString());
+        } catch (ServiceException  ex) {
+            // Cause exception
+            System.err.println("Cause = " + ex.getCause());
 
+        }
+    }
+
+}
--- Excpetion_Lab/src/services/CalculationService.java
+++ Excpetion_Lab/src/services/CalculationService.java
@@ -2,13 +2,15 @@ package services;
 
 import excpetions.ServiceException;
 
+import java.util.IllegalFormatException;
+
 /**
  * Created by Andrew on 12.12.2016.
  */
 public class CalculationService {
     public Boolean IsPlusResult (Integer firstNumber, Integer secondNumber) throws ServiceException {
         if(firstNumber == null | secondNumber == null)
-            throw new ServiceException("Numbers cannot be null!");
+            throw new ServiceException("Numbers cannot be null!", new IllegalArgumentException("Illegal arguments (Integer is null)"));
 
         Integer result = firstNumber + secondNumber;
 
